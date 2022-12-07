92f8611fae4530aea646dc066dbdf8e35caeceae
MalcolmSmalls <smallsbeats@gmail.com>
2022-12-06 23:55:18 -0800

    Add state when clicked on image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
 import Image from "./Components/Image"
+import React from "react"
 
 function App() {
+
+  const [ spotlight, setSpotlight] = React.useState(
+    {x: 0, y: 0, picked: false})
+
+  function handleClick(event){
+    setSpotlight(prevSpotlight => {
+      return {...prevSpotlight, x: event.pageX, y:event.pageY, picked: true}
+    })
+    console.log(event)
+    // return (
+    //   <div className="location"></div>
+    // )
+  }
+
+  const styles = {
+    display: spotlight.picked ? 'block' : 'none',
+    position: "absolute",
+    top: spotlight.y,
+    right: spotlight.x
+  }
+
   return (
     <div className="App">
-      <Image />
+      <Image handleClick = {handleClick} style = {styles}/>
     </div>
   );
 }
--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,9 +1,10 @@
 import React from "react"
 
-export default function Image(){
+export default function Image(props){
     return (
         <div className="image-container">
-            <img src="./Waldo.png" />
+            <img src="./Waldo.png" onClick={props.handleClick}  alt = "Find Waldo" />
+            <div className="location" style={props.style}></div>
         </div>
     )
 }
